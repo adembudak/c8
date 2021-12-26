75e2676ef27c1574ee675315e764ac13b802b3c9
vanilla milkshake <vanillamilkshakepls@gmail.com>
2021-12-26 22:56:14 +0300

    Fix buggy instructions

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 #### A slightly buggy chip-8 emulator
 
+Passes bc_test and instruction tests.
+
 ```shell
-$ c++ main.cpp -o c8
+$ c++ main.cpp -std=c++2a -o c8
 ```
 
 ----
 [![License: MIT](https://img.shields.io/badge/license-MIT-%23373737)](https://opensource.org/licenses/MIT)
-
 
--- main.cpp
+++ main.cpp
@@ -1,25 +1,28 @@
-#include <cstdint>
-#include <fstream>
-#include <algorithm>
-#include <array>
-#include <stack>
+// clang-format off
+
 #include <random>
-#include <iostream>
-#include <cassert>
-#include <chrono>
 #include <thread>
+#include <chrono>
+#include <cstdint>
+#include <array>
+#include <algorithm>
+#include <stack>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
 
 int main(int argc, const char *argv[]) {
   if (argc < 2) return 1;
+
   std::random_device rd;
   std::mt19937 engine{rd()};
-
-  using namespace std::this_thread;
-  using namespace std::chrono_literals;
 
   using int_distrib_t = std::uniform_int_distribution<int>;
   using interval = int_distrib_t::param_type;
   int_distrib_t rand;
+
+  using namespace std::this_thread;
+  using namespace std::chrono_literals;
 
   using byte = std::uint8_t;
   using u8 = std::uint8_t;
@@ -30,27 +33,31 @@ int main(int argc, const char *argv[]) {
   const bool jump = true;
   const bool load_store = false;
 
-  constexpr byte font[]{0xF0, 0x90, 0x90, 0x90, 0xF0, 0x20, 0x60, 0x20, 0x20, 0x70, 0xF0, 0x10,
-                        0xF0, 0x80, 0xF0, 0xF0, 0x10, 0xF0, 0x10, 0xF0, 0x90, 0x90, 0xF0, 0x10,
-                        0x10, 0xF0, 0x80, 0xF0, 0x10, 0xF0, 0xF0, 0x80, 0xF0, 0x90, 0xF0, 0xF0,
-                        0x10, 0x20, 0x40, 0x40, 0xF0, 0x90, 0xF0, 0x90, 0xF0, 0xF0, 0x90, 0xF0,
-                        0x10, 0xF0, 0xF0, 0x90, 0xF0, 0x90, 0x90, 0xE0, 0x90, 0xE0, 0x90, 0xE0,
-                        0xF0, 0x80, 0x80, 0x80, 0xF0, 0xE0, 0x90, 0x90, 0x90, 0xE0, 0xF0, 0x80,
-                        0xF0, 0x80, 0xF0, 0xF0, 0x80, 0xF0, 0x80, 0x80};
+  const bool white = true;
+  const bool black = false;
+
+  constexpr byte font[]{
+      0xF0, 0x90, 0x90, 0x90, 0xF0, 0x20, 0x60, 0x20, 0x20, 0x70, 0xF0, 0x10,
+      0xF0, 0x80, 0xF0, 0xF0, 0x10, 0xF0, 0x10, 0xF0, 0x90, 0x90, 0xF0, 0x10,
+      0x10, 0xF0, 0x80, 0xF0, 0x10, 0xF0, 0xF0, 0x80, 0xF0, 0x90, 0xF0, 0xF0,
+      0x10, 0x20, 0x40, 0x40, 0xF0, 0x90, 0xF0, 0x90, 0xF0, 0xF0, 0x90, 0xF0,
+      0x10, 0xF0, 0xF0, 0x90, 0xF0, 0x90, 0x90, 0xE0, 0x90, 0xE0, 0x90, 0xE0,
+      0xF0, 0x80, 0x80, 0x80, 0xF0, 0xE0, 0x90, 0x90, 0x90, 0xE0, 0xF0, 0x80,
+      0xF0, 0x80, 0xF0, 0xF0, 0x80, 0xF0, 0x80, 0x80};
 
   std::array<byte, 4 * 1024> ram{};
   std::copy(std::begin(font), std::end(font), std::begin(ram));
 
   constexpr u16 start_address = 0x200;
   std::ifstream fin{argv[1]};
-  std::copy(std::istreambuf_iterator<char>(fin), {}, std::begin(ram) + start_address);
+  std::copy(std::istreambuf_iterator(fin), {}, std::begin(ram) + start_address);
 
   u16 PC{start_address};
   u16 I{};
 
   using screenline = std::array<bool, 64>;
   using screen_t = std::array<screenline, 32>;
-  screen_t screen;
+  screen_t screen{};
 
   std::stack<u16> stack;
 
@@ -74,17 +81,16 @@ int main(int argc, const char *argv[]) {
     const u8 x = (op & 0x0f00) >> 8;
     const u8 y = (op & 0x00f0) >> 4;
 
-    // clang-format off
     switch ((op & 0xf000) >> 12) {
       case 0x0:
         switch (op & 0x00ff) {
-          case 0xe0: { for (auto &line : screen) line.fill(false); } break;
+          case 0xe0: { for (auto &line : screen) line.fill(black); frame_changed = true; } break;
           case 0xee: PC = stack.top(); stack.pop(); break;
         }
         break;
 
-      case 0x1: PC = nnn; break;
-      case 0x2: stack.push(PC); PC = nnn; break;
+      case 0x1: PC = nnn; PC -= 2; break;
+      case 0x2: stack.push(PC); PC = nnn; PC -= 2; break;
       case 0x3: if (VX[x] == nn) { PC += 2; } break;
       case 0x4: if (VX[x] != nn) { PC += 2; } break;
       case 0x5: if (VX[x] == VX[y]) { PC += 2; } break;
@@ -98,22 +104,21 @@ int main(int argc, const char *argv[]) {
           case 0x3: VX[x] ^= VX[y]; break;
           case 0x4: VF = (VX[x] + VX[y]) > 0xff; VX[x] += VX[y]; break;
           case 0x5: VF = VX[x] > VX[y]; VX[x] -= VX[y]; break;
-          case 0x6: if (shift) { VX[x] = VX[y]; } VF = VX[x] & 0b0000'0001; VX[x] >>= 1; break;
+          case 0x6: VF = bool(VX[x] & 0b0000'0001); if (shift) { VX[x] = VX[y] >> 1; } else { VX[x] >>= 1; } break;
           case 0x7: VF = VX[y] > VX[x]; VX[x] = VX[y] - VX[x]; break;
-          case 0xe: if (shift) { VX[x] = VX[y]; } VF = VX[x] & 0b1000'0000; VX[x] <<= 1; break;
+          case 0xe: VF = bool(VX[x] & 0b1000'0000); if (shift) { VX[x] = VX[y] << 1; } else { VX[x] <<= 1; } break;
         }
         break;
 
       case 0x9: if (VX[x] != VX[y]) { PC += 2; } break;
       case 0xa: I = nnn; break;
-      case 0xb: if (jump) { PC = VX[0] + nnn; } else { PC = VX[x] + nn; } break;
+      case 0xb: if (jump) { PC = VX[0] + nnn; } else { PC = VX[x] + nn; } PC -= 2; break;
       case 0xc: VX[x] = nn & rand(engine, interval{0, 0xff}); break;
       case 0xd: {
         const u8 X = VX[x] % 64;
         const u8 Y = VX[y] % 32;
         const u8 N = n;
 
-        // clang-format on
         VF = 0;
         for (std::size_t row = 0; row < N; ++row) {
           if (Y + row > 31) break;
@@ -121,17 +126,17 @@ int main(int argc, const char *argv[]) {
           const byte tile_line_on_memory = ram[I + row];
           if (tile_line_on_memory == 0) continue;
 
-          for (std::size_t column = 0; column < 7; ++column) {
+          for (std::size_t column = 0; column < 8; ++column) {
             if (X + column > 63) break;
 
             bool &tile_pixel_on_screen = screen[Y + row][X + column];
 
             if (tile_line_on_memory & (0b1000'0000 >> column)) {
-              if (tile_pixel_on_screen) {
+              if (tile_pixel_on_screen == white) {
                 VF = 1;
-                tile_pixel_on_screen = false;
+                tile_pixel_on_screen = black;
               } else {
-                tile_pixel_on_screen = true;
+                tile_pixel_on_screen = white;
               }
               frame_changed = true;
             }
@@ -139,30 +144,26 @@ int main(int argc, const char *argv[]) {
         }
       } break;
 
-        // clang-format off
       case 0xe:
         switch (op & 0x00ff) {
-          case 0x9e: if (keypad[VX[x]] & 0x0f) { PC += 2; } break;
-          case 0xa1: if (!keypad[VX[x] & 0x0f]) { PC += 2; } break;
+          case 0x9e: if (keypad[VX[x]]) { PC += 2; } break;
+          case 0xa1: if (!keypad[VX[x]]) { PC += 2; } break;
         }
 
         break;
 
       case 0xf:
         switch (op & 0x00ff) {
-          case 0x07: delay_timer = VX[x]; break;
-          case 0x15: VX[x] = delay_timer; break;
-          case 0x18: VX[x] = sound_timer; break;
-          case 0x1e: if ((I + VX[x]) > 0x0fff) { VF = 1; } I += VX[x]; break;
-          case 0x0a: // blocking key input
-            //  VX[x] = key-input-in-hexa;
-            assert(false);
-            break;
+          case 0x07: VX[x] = delay_timer; break;
+          case 0x15: delay_timer = VX[x]; break;
+          case 0x18: sound_timer = VX[x]; break;
+          case 0x1e: VF = (I + VX[x]) > 0x0fff; I += VX[x]; break;
+          case 0x0a: VX[x] = 0xFF & std::getchar(); break;
 
-          case 0x29: I = font[VX[x]]; break;
-          case 0x33: ram[I] = VX[x] / 100; ram[I + 1] = (VX[x] % 100) / 10; ram[I + 2] = VX[x] % 10; break;
-          case 0x55: std::copy(begin(VX), begin(VX) + x + 1, begin(ram) + I); if (!load_store) { I += (x + 1); } break;
-          case 0x65: std::copy(begin(ram) + I, begin(ram) + I + x + 1, begin(VX)); if (!load_store) { I += (x + 1); } break;
+          case 0x29: I = VX[x]*5; break;
+          case 0x33: ram[I] = VX[x] / 100; ram[I + 1] = (VX[x] / 10) % 10; ram[I + 2] = VX[x] % 10; break;
+          case 0x55: std::copy_n(begin(VX), x + 1, begin(ram) + I); if (load_store) { I += (x + 1); } break;
+          case 0x65: std::copy_n(begin(ram) + I, x + 1, begin(VX)); if (load_store) { I += (x + 1); } break;
         }
 
         break;
@@ -172,18 +173,13 @@ int main(int argc, const char *argv[]) {
     if (delay_timer > 0) { --delay_timer; }
     if (sound_timer > 0) { std::cout << '\a'; --sound_timer; }
 
-    //  std::cout << std::string('\n', 20);
     if (frame_changed) {
       frame_changed = false;
-      std::cout << '\n';
+      std::cout << std::string('\n', 10);
 
       for (const auto &line : screen)
-        for (std::cout << '\n'; const bool pixel : line)
-          if (pixel)
-            std::cout << "\u2b1b";
-          else
-            std::cout << "\u2b1c";
-      
+        for (std::cout << '\n'; bool pixel : line)
+          std::cout << (pixel ? "\u2b1c" : "\u2b1b");
     }
 
     sleep_for(16ms);
